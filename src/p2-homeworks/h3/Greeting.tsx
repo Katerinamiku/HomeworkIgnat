241e5ca53a8c281999b70e0b664f9d8670753962
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-06-10 16:43:47 +0200

    third HW task

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,33 @@
-import React from 'react'
+import React, {ChangeEvent, KeyboardEvent, FocusEvent} from 'react'
 import s from './Greeting.module.css'
 
+
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (event: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
+    onKeyPressHandler: (event: KeyboardEvent<HTMLInputElement>) => void
+    onFocusHandler: (event: FocusEvent<HTMLInputElement>) => void
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, setNameCallback, addUser, error, totalUsers, onKeyPressHandler, onFocusHandler} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = error !== '' ? s.error : s.nonError;// need to fix with (?:)
+    const warningClass = s.warning
 
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+            <div>
+                <input value={name} className={inputClass} onChange={setNameCallback} onFocus={onFocusHandler} onKeyDown={onKeyPressHandler}/>
+                <button onClick={addUser}>add</button>
+                <span>{totalUsers}</span>
+            </div>
+            <div><span className={warningClass}>{error}</span></div>
+
         </div>
     )
 }
