43c19723d4f1e54d5a204612d4ab97907ff07182
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-06-27 23:27:10 +0200

    fifth HW task -update

--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -23,10 +23,7 @@ function Pages() {
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-
-                {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                <Route path={'/*'} element={<Error404/>}/>
-
+                <Route path={'*'} element={<Error404/>}/>
             </Routes>
         </div>
     )
