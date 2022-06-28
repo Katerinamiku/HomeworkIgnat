0153bc226e50386e68cdf74a1cc9b14fc6688bc7
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-06-28 13:53:24 +0200

    fifth HW task -update burger menu

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,23 +1,30 @@
+import {Squash as Hamburger} from 'hamburger-react';
 import React, {useState} from 'react'
 import {NavLink} from "react-router-dom";
 import s from './HW.module.css'
 
 function Header() {
 
+    const [isActive, setIsActive] = useState<boolean>(false)
+    const activeMenuStyle = isActive && s.menuActive
+    const showMenu = () => {
+        setIsActive(true)
+    }
+    const hideMenu = ()=> {
+        setIsActive(false)
+    }
+
     return (
         <div className={s.navMenu}>
-        <div className={s.nav}>
-            <input type={'checkbox'} />
-            <span></span>
-            <span></span>
-            <span></span>
-            <div className= {s.menu}>
-                <li><NavLink to='/pre-junior'> Pre-Junior </NavLink></li>
-                <li><NavLink to='/junior'>Junior</NavLink></li>
-                <li><NavLink to='/junior-plus'>Junior+</NavLink></li>
+            <div className={s.nav} onMouseEnter={showMenu} onMouseLeave={hideMenu}>
+                <Hamburger color={"#03e9f4"} size={20} toggled={isActive} toggle={setIsActive}/>
+                <div className= {`${s.menu} ${activeMenuStyle}`}>
+                    <li><NavLink to='/pre-junior'> Pre-Junior </NavLink></li>
+                    <li><NavLink to='/junior'>Junior</NavLink></li>
+                    <li><NavLink to='/junior-plus'>Junior+</NavLink></li>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
