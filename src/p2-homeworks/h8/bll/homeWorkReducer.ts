6f86c241c9a8dad1a00cbbf19aaaf9534b3cf04c
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-07-18 15:14:40 +0200

    eight HW task

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,44 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type SortPeopleAT = {
+    type: 'sort',
+    payload: string
+}
+
+type CheckPeopleAT = {
+    type: 'check'
+    payload: number
+}
+
+export type ActionType = SortPeopleAT | CheckPeopleAT;
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            return action.payload === 'up'
+                ? [...state].sort((a, b) => a.name.localeCompare(b.name))
+                : action.payload === 'down'
+                    ? [...state].sort((a, b) => a.name.localeCompare(b.name)).reverse()
+                    : state;
         }
         case 'check': {
-            // need to fix
-            return state
+            return [...state].filter(p => p.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
+    }
+}
+
+export const SortPeopleAC = (payload: string): SortPeopleAT => {
+    return {
+        type: 'sort',
+        payload
+    }
+}
+
+export const CheckPeopleAC = (payload: number): CheckPeopleAT => {
+    return {
+        type: 'check',
+        payload
     }
 }
