af0d252599bb1bc4f9e7bb1066214be6f5b5dee8
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-08-01 16:20:08 +0200

    10 HW task - Loader

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import Loader from "./Loader";
+import s from './Loader.module.css';
+
+
+export type LoadingType = {
+    loadingValue: boolean
+}
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+    const loadingState = useSelector<AppStoreType, LoadingType>(state => state.loading)
+    const dispatch = useDispatch();
+
+    const loading = loadingState.loadingValue
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
+        dispatch(loadingAC(true));
+        setTimeout(() => dispatch(loadingAC(false)), 3000)
         console.log('loading...')
     };
 
@@ -17,22 +29,25 @@ function HW10() {
             homeworks 10
 
             {/*should work (должно работать)*/}
-            {loading
-                ? (
-                    <div>крутилка...</div>
-                ) : (
-                    <div>
-                        <SuperButton onClick={setLoading}>set loading...</SuperButton>
-                    </div>
-                )
+            <div className={s.loadingWindow}>
+                {loading
+                    ? (
+                        <div>
+                            <Loader/>
+                        </div>
+                    ) : (
+                        <div>
+                            <SuperButton onClick={setLoading}>set loading...</SuperButton>
+                        </div>
+                    )
+                }
+            </div>
+                <hr/>
+                {/*для личного творчества, могу проверить*/}
+                {/*<Alternative/>*/}
+                <hr/>
+            </div>
+            )
             }
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<Alternative/>*/}
-            <hr/>
-        </div>
-    )
-}
-
-export default HW10
+            export default HW10
