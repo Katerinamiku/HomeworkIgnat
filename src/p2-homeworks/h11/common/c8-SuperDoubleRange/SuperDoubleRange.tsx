b31ea5018191282c20299e9ba0f0b42e535b9579
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-08-10 19:34:18 +0200

    11 HW - Range

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,49 @@
 import React from 'react'
+import s from "./SuperDoubleRange.module.css";
+import {Slider} from "@mui/material";
+
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+    onChangeRange: (value: [number, number]) => void
+    value: [number, number]
 }
+
+const minDistance = 1;
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
-        // min, max, step, disable, ...
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+
+    const handleChange1 = (
+        event: Event,
+        newValue: number | number[],
+        activeThumb: number,
+    ) => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+
+        if (activeThumb === 0) {
+            onChangeRange([Math.min(newValue[0], value[1] - minDistance), value[1]]);
+        } else {
+            onChangeRange([value[0], Math.max(newValue[1], value[0] + minDistance)]);
+        }
+    };
+
 
     return (
-        <>
-            DoubleRange
-        </>
-    )
+        <div className={s.sliderWrapper}>
+            <Slider getAriaLabel={() => 'Minimum distance'}
+                    value={value}
+                    onChange={handleChange1}
+                    valueLabelDisplay="auto"
+                    disableSwap
+                    size={'small'}
+                    sx={{color: '#03e9f4'}}/>
+        </div>
+    );
 }
 
 export default SuperDoubleRange
